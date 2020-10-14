8067ee9a0e89e29b6d3c4c4d1d387f6b7fe2efb0
Michael <bausanomichal@gmail.com>
2020-10-14 02:54:21 +0100

    There's a bug with mutability of some of the properties.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,10 @@
 import { Vec3 } from "vec3";
 import { createBot, Bot, ControlState } from "mineflayer";
 
-// When we calculate new velocity we might end up with miniscule updates to our
-// velocity. Since we must control the bot via the control "arrows", we will
-// ignore all adjustments whose absolute value is smaller than this.
-const VELOCITY_DELTA_THRESHOLD = 0.1;
 // How many blocks can a prey see.
-const PREY_VISION_RADIUS = 10;
+const PREY_VISION_RADIUS = 20;
 // Prey will attempt to keep distance from each other at least this many blocks.
-const PREY_AVOID_RADIUS = 3;
+const PREY_AVOID_RADIUS = 5;
 // TODO
 const PREY_MAX_SPEED = 5;
 // TODO
@@ -23,7 +19,7 @@ const ESCAPE_FORCE = 3.0;
 
 const host = "localhost";
 const port = 35627;
-const preyCount = 3;
+const preyCount = 1;
 
 (async () => {
     // Creates fleet of prey.
@@ -42,20 +38,22 @@ const preyCount = 3;
             (bot) => new Promise((resolve) => bot.addListener("spawn", resolve))
         )
     );
-
-    console.log(`${preyCount} prey bots connected. Beginning boids behavior.`);
+    console.log(`${preyCount} prey bots connected.`);
 
     // Bots always go forward. We just change where they look.
     for (let bot of prey) {
-        bot.setControlState("forward", true)
+        // bot.setControlState("forward", true)
     }
 
-    // Game loop
+    console.log("Beginning boids behavior.");
     while (true) {
         // TODO
-        let msDelta = 50;
+        let msDelta = 100;
+        await new Promise((resolve) => setInterval(resolve, msDelta));
         for (let bot of prey) {
-            const { position, velocity } = bot.entity;
+            let { position, velocity } = bot.entity;
+            position = position.clone();
+            position = velocity.clone();
 
             // How many other prey is nearby.
             let flockmates = 0;
@@ -68,29 +66,38 @@ const preyCount = 3;
             let separationDir = vec3Splat(0);
 
             for (let entity of Object.values(bot.entities)) {
-                const offset = position.subtract(entity.position);
+                const offset = position.clone().subtract(entity.position);
                 // Just making sure this quantity is not zero for convenience.
                 const sqDist =
                     position.distanceSquared(entity.position) || 0.0001;
+
+                console.log(
+                    entity.username,
+                    bot.username,
+                    position,
+                    sqDist,
+                    entity.position
+                );
 
                 // Skip entities too far away for us to care or are animals.
                 if (sqDist > PREY_VISION_RADIUS || !entity.username) {
                     continue;
                 }
 
-                if (entity.username.startsWith("prey")) {
+                if (
+                    entity.type === "player" ||
+                    entity.username.startsWith("prey")
+                ) {
                     flockmates++;
                     // Used to calculate affect of alignment force. See below.
-                    headingDir = headingDir.add(entity.velocity);
+                    headingDir.add(entity.velocity);
                     // Used to calculate affect of cohesion force. See below.
-                    centerTotal = centerTotal.add(entity.position);
+                    centerTotal.add(entity.position);
 
                     // If prey is too close to each other, try change its direction
                     // so that they don't bump.
                     if (sqDist < PREY_AVOID_RADIUS * PREY_AVOID_RADIUS) {
-                        separationDir = separationDir.add(
-                            offset.scale(1 / sqDist)
-                        );
+                        separationDir.add(offset.scale(1 / sqDist));
                     }
                 } else if (entity.username.startsWith("predator")) {
                     // TODO: Maybe we can use "else" as all other usernames
@@ -110,23 +117,17 @@ const preyCount = 3;
                     .scale(1 / flockmates)
                     .subtract(position);
                 let cohesionForce = steerTowards(velocity, offsetToFlockCenter);
-                acceleration = acceleration.add(
-                    cohesionForce.scale(COHESION_FORCE)
-                );
+                acceleration.add(cohesionForce.scale(COHESION_FORCE));
 
                 // Aligns velocity vectors with nearby flockmates.
                 let alignmentForce = steerTowards(velocity, headingDir);
-                acceleration = acceleration.add(
-                    alignmentForce.scale(ALIGNMENT_FORCE)
-                );
+                acceleration.add(alignmentForce.scale(ALIGNMENT_FORCE));
 
                 // If there is some separation to be sustained with nearby
                 // flockmates, apply the force to the acceleration.
                 if (separationDir.volume()) {
                     let separationForce = steerTowards(velocity, separationDir);
-                    acceleration = acceleration.add(
-                        separationForce.scale(SEPARATION_FORCE)
-                    );
+                    acceleration.add(separationForce.scale(SEPARATION_FORCE));
                 }
             }
 
@@ -136,24 +137,11 @@ const preyCount = 3;
                 acceleration.scale(msDelta / 1000)
             );
 
-            // Now based on the new velocity we calculate which controls should
-            // we emit. We don't care about y direction as that's up/down.
-            //
-            //          x
-            //          |
-            //          |
-            // -z ------+------ z
-            //          |
-            //          |
-            //         -x
+            console.log(bot.username, newVelocity);
 
-            // Alternatively get position, add velocity, look at point. Always
-            // have steer on.
-
-            updateMovement(bot, newVelocity.x, "forward", "back")
-            updateMovement(bot, newVelocity.z, "left", "right")
-
-            console.log(bot.username, newVelocity.x, newVelocity.z);
+            // The bots always go forward. We just change the direction they
+            // look.
+            bot.lookAt(position.add(newVelocity));
         }
     }
 })();
@@ -169,23 +157,4 @@ function steerTowards(velocity: Vec3, force: Vec3): Vec3 {
 
 function vec3Splat(s: number): Vec3 {
     return new Vec3(s, s, s);
-}
-
-// Updates bots movement based on direction. If direction is positive, sets the
-// state to positive control string, otherwise negative.
-// Resets any active state if appropriate.
-function updateMovement(
-    bot: Bot,
-    direction: number,
-    positive: ControlState,
-    negative: ControlState
-) {
-    const shouldTurnOn = Math.abs(direction) < VELOCITY_DELTA_THRESHOLD;
-    if (shouldTurnOn) {
-        bot.setControlState(direction > 0 ? positive : negative, true);
-    } else {
-        // TODO: Do this only if we need to.
-        bot.setControlState(negative, false);
-        bot.setControlState(positive, false);
-    }
 }
